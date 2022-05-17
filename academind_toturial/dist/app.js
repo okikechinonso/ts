7c5bc17d50841b2025bbb1decdf51df5c9630c15
okikechinonso <chinonsookike@gmail.com>
2022-05-17 12:45:50 +0100

    Add config and npm package to the project

---
+++ academind_toturial/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("start serve successfully");
+function add(n1, n2) {
+    console.log(typeof n1 === 'number');
+    return n1 + n2;
+}
+console.log(add(2, 5));
---
+++ academind_toturial/src/app.ts
@@ -0,0 +1,7 @@
+console.log("start serve successfully");
+
+function add(n1:number, n2:number): number{
+    console.log(typeof n1 === 'number')
+    return n1 + n2;
+}
+console.log(add(2,5))
