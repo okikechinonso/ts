f50d2d9aafe3d77ff10699c1cdebd1ed015cb189
Admin <chinonsookike@gmail.com>
2022-05-18 08:06:54 +0100

    finish the academind

--- academind_toturial/dist/app.js
+++ academind_toturial/dist/app.js
@@ -5,3 +5,52 @@ function add(n1, n2) {
     return n1 + n2;
 }
 console.log(add(2, 5));
+//object 
+const person = {
+    name: "chinonso",
+    age: 15,
+    number: 78
+};
+let arr;
+arr = ["string"];
+//tuples
+const person1 = {
+    name: "chinonso",
+    age: 15,
+    number: 78
+};
+//enum
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+// add change the identifiers if you don't want to use the equal sign to reassign
+var Role2;
+(function (Role2) {
+    Role2[Role2["ADMIN"] = 3] = "ADMIN";
+    Role2[Role2["READ_ONLY"] = 4] = "READ_ONLY";
+    Role2[Role2["AUTHOR"] = 5] = "AUTHOR";
+})(Role2 || (Role2 = {}));
+const person2 = {
+    name: "chinonso",
+    age: 15,
+    number: 78
+};
+///unnions
+let names;
+names = "chinonso";
+function sub(input, input2) {
+    if (typeof input === 'number') {
+        return input + input2;
+    }
+}
+//never is used for functioon that never return anything and for infiniter loop
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code
+    };
+}
+generateError('An error occured', 500);
--- academind_toturial/src/app.ts
+++ academind_toturial/src/app.ts
@@ -5,3 +5,80 @@ function add(n1:number, n2:number): number{
     return n1 + n2;
 }
 console.log(add(2,5))
+
+//object 
+const person: {
+    name: string,
+    age: number,
+    number: number,
+} = {
+    name: "chinonso",
+    age: 15,
+    number: 78
+}
+
+let arr: string[];
+arr = ["string"];
+
+//tuples
+
+const person1: {
+    name: string,
+    age: number,
+    number: number,
+    role?: [number, string], //tuples
+} = {
+    name: "chinonso",
+    age: 15,
+    number: 78
+}
+//enum
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR,
+}
+
+// add change the identifiers if you don't want to use the equal sign to reassign
+enum Role2 {
+    ADMIN =3,
+    READ_ONLY,
+    AUTHOR,
+}
+
+const person2: {
+    name: string,
+    age: number,
+    number: number,
+    role?: Role //tuples
+} = {
+    name: "chinonso",
+    age: 15,
+    number: 78
+}
+
+///unnions
+
+let names: string | boolean;
+names = "chinonso"
+
+function sub(input: number|boolean, input2: number){
+    if (typeof input === 'number'){
+        return input+input2
+    }
+    
+}
+
+// type alias
+
+type Combineable = number | string;
+
+
+//never is used for functioon that never return anything and for infiniter loop
+function generateError(message: string, code: number): never{
+    throw{
+        message: message,
+         errorCode: code
+    }
+}
+generateError('An error occured',500)
